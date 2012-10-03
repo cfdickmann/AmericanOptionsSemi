09d022f2df3e5d229025d7f13762eab05eb01045
rdr <rdr@ubuntu.(none)>
2012-10-03 19:15:56 +0200

    ja

--- Debug/hochladen.sh
+++ Debug/hochladen.sh
@@ -1,1 +1,1 @@
-scp  -r -P 2551 ../quellen/*  dickmann@the-brain.math.uni-duisburg.de:~/TEST/AmericanOptionsSemi/quellen/
+scp  -r -P 2551 ../quellen/*  dickmann@the-brain.math.uni-duisburg.de:~/workspace/AmericanOptionsSemi/quellen/
